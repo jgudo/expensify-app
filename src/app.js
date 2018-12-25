421176496b275316d615473126b9ce25f4fc5c09
Julius Guevarra <juliusguevarra101@gmail.com>
2018-12-25 10:56:07 -0800

    Setup production build and server

--- src/app.js
+++ src/app.js
@@ -3,15 +3,9 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import {addExpense} from './actions/expenses';
-import {setTextFilter} from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import 'react-dates/lib/css/_datepicker.css';
 import './styles/style.scss';
-
-
-
 
 const store = configureStore();
 const jsx = (
