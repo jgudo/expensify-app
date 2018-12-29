eeab00a1505e5dc753e3797b103e96b66a8141e3
Julius Guevarra <juliusguevarra101@gmail.com>
2018-12-29 12:57:51 -0800

    modified subtitle

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -7,7 +7,7 @@ export const LoginPage = ({startLogin}) => (
     <div className="box-layout">
         <div className="box-layout__box">
             <h1 className="box-layout__title">Expensify</h1>
-            <p>Get your expenses under control</p>
+            <p>Make your expenses under control</p>
             <button onClick={startLogin} className="button button--stretched button--social button--google">
                 <span>Login with Google</span> 
                 <span><FontAwesomeIcon icon={['fab', 'google']} /></span>
